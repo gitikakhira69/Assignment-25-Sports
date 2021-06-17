60defc386a08e622728d240f912e689758c387be
gitikakhira69 <79353610+gitikakhira69@users.noreply.github.com>
2021-06-17 13:33:42 +0530

    Add files via upload

---
+++ 25-Sports Assignment/iplresult.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBrqFwElKwcY4HSBu4IioMng2ERDDvymAw",
+    authDomain: "ipl-results-624fb.firebaseapp.com",
+    databaseURL: "https://ipl-results-624fb-default-rtdb.firebaseio.com",
+    projectId: "ipl-results-624fb",
+    storageBucket: "ipl-results-624fb.appspot.com",
+    messagingSenderId: "301814242962",
+    appId: "1:301814242962:web:da2e74e4c3a0794221104b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
